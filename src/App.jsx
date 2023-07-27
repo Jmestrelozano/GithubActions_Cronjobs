577dc07799d0f11e86b7a202efa2de3b3d8e1780
Jmestrelozano <jmestrelozano@gmail.com>
2023-07-27 07:29:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a
+          rel="noreferrer"
+          href="https://docs.github.com/es/actions/learn-github-actions/understanding-github-actions"
+          target="_blank"
+        >
+          <img
+            src={
+              "https://user-images.githubusercontent.com/56113566/144758957-746eeff1-a859-4a02-8a6e-3b1f660c622c.png"
+            }
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>Github actions and Cronjobs</h1>
+    </>
+  );
+}
+
+export default App;
